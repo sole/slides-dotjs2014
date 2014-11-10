7b541fe1f6a8c620d8ff8f612552cf36360c04ed
sole <sole@mozilla.com>
2014-11-10 11:26:52 +0000

    start up following style guide

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// Minimal require style by sole because I hate the default bespoke style
+require('bespoke').from('article', [
+	require('bespoke-theme-nebula')(),
+	require('bespoke-keys')(),
+	require('bespoke-touch')(),
+	require('bespoke-bullets')('li, .bullet'),
+	require('bespoke-backdrop')(),
+	require('bespoke-scale')(),
+	require('bespoke-hash')(),
+	require('bespoke-progress')(),
+	require('bespoke-forms')()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
