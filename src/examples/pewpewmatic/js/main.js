073c21cc9c252e5126d39b1af2ecf833c111d3e7
sole <sole@mozilla.com>
2014-11-11 11:52:34 +0000

    up to samples

---
+++ src/examples/pewpewmatic/js/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', function() {
+  
+  var samplePath = '../_data/pewpew.ogg';
+  var button = document.querySelector('button');
+  var context = new AudioContext();
+  var buffer;
+
+  var request = new XMLHttpRequest();
+  request.open('GET', samplePath, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    context.decodeAudioData(request.response, loadedCallback, errorCallback);
+  };
+  request.send();
+
+  function loadedCallback(data) {
+    buffer = data;
+    addListeners();
+  }
+
+  function errorCallback() {
+    alert('No PEW PEW for you');
+  }
+
+  function addListeners() {
+    button.addEventListener('click', pewpew);
+    window.addEventListener('keyup', function(ev) {
+      if(ev.keyCode !== 32) {
+        return;
+      }
+      pewpew();
+    });
+  }
+
+  function pewpew() {
+    var bs = context.createBufferSource();
+    bs.buffer = buffer;
+    bs.connect(context.destination);
+    bs.start();
+  }
+
+
+});
